015ab25ecedf1ea7d308e6c49b175d429a1bfad1
Rana Hisham Foad <rana@megatrust.co>
2022-03-01 11:30:13 +0200

    ahmed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'td-form-app';
+  submitted: boolean = false;
+
+  @ViewChild('f') formData: NgForm;
+  userData = {
+    mail: '',
+    subscription: '',
+    password: '',
+  };
+
+  submit() {
+    console.log(this.formData);
+
+    this.userData.mail = this.formData.form.value.mail;
+    this.userData.subscription = this.formData.form.value.subscription;
+    this.userData.password = this.formData.form.value.password;
+    this.submitted = true;
+    this.formData.reset();
+  }
 }
